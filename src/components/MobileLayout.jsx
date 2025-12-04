f3b893e684d5d56eac95c0eb4287588db832b9f5
marinadotnet <msichova@outlook.com>
2025-12-04 22:45:48 +0800

    added DecorativeTopComponentMob.jsx to simulate the main menu and toolbar of the Visual Studio for small screens, updated index.css with new styles for DecorativeTopComponentMob.jsx element and MobileLayout.jsx to display DecorativeTopComponentMob.jsx

--- src/components/MobileLayout.jsx
+++ src/components/MobileLayout.jsx
@@ -1,14 +1,20 @@
 import NavBar from "./jointLayoutComponents/NavBar";
+import DecorativeTopComponentMob from "./mobileLaoyoutComponents/DecorativeTopComponentMob";
 
 import {HashRouter as Router, Routes, Route} from "react-router-dom";
-
 
 export default function MobileLayout(){
     return(
         <Router>
-            <NavBar />
-            <h1>This is layout for Mobile</h1>
-            <p>Will be added later.</p>
+            <div className="page-layout">
+
+                <DecorativeTopComponentMob />
+
+                <NavBar />
+                <h1>This is layout for Mobile</h1>
+                <p>Will be added later.</p>
+            </div>
+            
         </Router>
     );
 }
---
+++ src/components/mobileLaoyoutComponents/DecorativeTopComponentMob.jsx
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @returns 
+ * Simulation of UI for main menu and toolbar of Visual Studio for small screens.
+ * Currently only for decoration purposses.
+ */
+export default function DecorativeTopComponentMob(){
+    return(
+        <div className="decorative-top-mobile">
+            <span className="text-primary">MarinaDotNet.Portfolio</span>
+        </div>
+    );
+}
