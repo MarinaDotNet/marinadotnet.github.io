9d228373fc6c69bbe1f67c763a51302eff3b3870
marinadotnet <msichova@outlook.com>
2025-12-08 14:15:03 +0800

    added LightThemeIcon.jsx function that returns SVG element, the 'half moon'.

---
+++ src/components/jointLayoutComponents/LightThemeIcon.jsx
@@ -0,0 +1,10 @@
+/**
+ * @returns SVG light theme icon element, the 'half moon'.
+ */
+export default function LightThemeIcon(){
+    return(
+        <svg width='30' height='30'>
+            <path fill='#B88A13' d='M 23, 5 A 12 12 0 1 0 23, 25 A 12 12 0 0 1 23, 5'/>
+        </svg>
+    );
+}
