1b54687ca24c3c7cc1b9298e3cb39ba9013a6aeb
marinadotnet <msichova@outlook.com>
2025-12-08 16:12:01 +0800

    added Skills.jsx and updated DesktopLayout.jsx to display Sills element

--- src/components/DesktopLayout.jsx
+++ src/components/DesktopLayout.jsx
@@ -6,6 +6,7 @@ import DecorativeTopPageLine from "./desktopLayoutComponents/DecorativeTopPageLi
 
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
+import Skills from "./pages/Skills";
 
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -26,6 +27,7 @@ export default function DesktopLayout(){
                             <Routes>
                                 <Route path="/" element={<Home />} />
                                 <Route path="/projects" element={<Projects />} />
+                                <Route path="/skills" element={<Skills />} />
                             </Routes>
                         </div>
                     </div>
