cbd00a9ceaf2a35af482b4b7c08af404522dc6f7
marinadotnet <msichova@outlook.com>
2025-12-17 16:49:28 +0800

    update SideBar.jsx added links to the buttons to visit at social media and to download CV in different formats.

--- src/components/mobileLaoyoutComponents/SideBar.jsx
+++ src/components/mobileLaoyoutComponents/SideBar.jsx
@@ -21,45 +21,71 @@ export default function SideBar(){
             </a>
             
             <div className="download-wrapper">
-                <button className="download-btn" title="Download PNG resume">
+                <button 
+                className="download-btn" 
+                title="Visit me at GitHub"
+                onClick={() => {window.open("https://github.com/marinadotnet", "_blank");}}
+                >
                     <Github className="icon" />
                 </button>
             </div>
 
             <div className="download-wrapper">
-                <button className="download-btn" title="Download PNG resume">
+                <button 
+                className="download-btn" 
+                title="Visit me at LinkedIn"
+                onClick={() => {window.open("https://www.linkedin.com/in/marina-sichova-333599281", "_blank");}}
+                >
                     <Linkedin className="icon" />
                 </button>
             </div>
 
             <div className="download-wrapper">
-                <button className="download-btn" title="Download PNG resume">
+                <button 
+                className="download-btn" 
+                title="Send me an email"
+                onClick={() => {window.location.href="mailto: msichova@outlook.com";}}
+                >
                     <Mail className="icon" />
                 </button>
             </div>
 
             <div className="download-wrapper">
-                <button className="download-btn" title="Download PNG resume">
+                <button 
+                className="download-btn" 
+                title="Visit me at Facebook"
+                onClick={() => {window.open("https://www.facebook.com/msichova/", "_blank");}}
+                >
                     <Facebook className="icon" />
                 </button>
             </div>
             <div className="full-line"></div>
             <div className="download-wrapper">
-                <button className="download-btn" title="Download PDF resume">
+                <button 
+                className="download-btn" 
+                onClick={() => {window.open("https://raw.githubusercontent.com/MarinaDotNet/marinadotnet.github.io/main/static/files/cv.pdf", "_blank");}}
+                >
                     <Download className="icon" />
                     <span>PDF</span>
                 </button>
             </div>
 
             <div className="download-wrapper">
-                <button className="download-btn" title="Download DOC resume">
+                <button 
+                className="download-btn" 
+                onClick={() => {window.open("https://raw.githubusercontent.com/MarinaDotNet/marinadotnet.github.io/main/static/files/cv.docx", "_blank", "noopener,noreferrer");}}
+                >
                     <Download className="icon" />
                     <span>DOC</span>
                 </button>
             </div>
 
             <div className="download-wrapper">
-                <button className="download-btn" title="Download PNG resume">
+                <button 
+                className="download-btn" 
+                title="Download PNG resume"
+                onClick={()  => {window.open("https://raw.githubusercontent.com/MarinaDotNet/marinadotnet.github.io/main/static/files/cv.png", "_blank", "noopener,noreferrer");}}
+                >
                     <Download className="icon" />
                     <span>PNG</span>
                 </button>
