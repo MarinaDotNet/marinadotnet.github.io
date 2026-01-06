b68ad18e1dc4123506bbcb70ba1e1cbc9c28128a
marinadotnet <msichova@outlook.com>
2026-01-06 16:11:39 +0800

    refactor: Contact.jsx to support both view modes and updated content, updated index.css with new style.

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,42 +1,112 @@
-import {NumberedCodeBloc, CodeKeyWords, CodeValues, ClassName, Tab, SingleComment} from "../jointLayoutComponents/CodeTokens";
+import {NumberedCodeBloc, CodeKeyWords, CodeValues, ClassName, Tab, SingleComment, CodeIdentifier, Comment} from "../jointLayoutComponents/CodeTokens";
 
-export default function Contact (){
-    return (
-        <NumberedCodeBloc children={[
+export default function Contact ({viewMode}){
+    if(viewMode === 'developer'){
+        return (
+            <NumberedCodeBloc children={[
             <><CodeKeyWords>using</CodeKeyWords> Human.Interaction;</>,
             <><CodeKeyWords>using</CodeKeyWords> GitHub.Connections;</>,
             <><CodeKeyWords>using</CodeKeyWords> Async.Communication;</>,
             <><CodeKeyWords>using</CodeKeyWords> OpenPorts._443;</>,
             <><CodeKeyWords>using</CodeKeyWords> Networking.Protocols;</>,
             <br/>,
-            <><CodeKeyWords>namespace</CodeKeyWords> MarinaDotNet;</>,
+            <><CodeKeyWords>namespace</CodeKeyWords> <CodeIdentifier>MarinaDotNet</CodeIdentifier>;</>,
             <br />,
-            <><CodeKeyWords>public static class</CodeKeyWords> Contact</>,
+            <Comment>
+                My contact info:,
+                Email: <a className="link-in-comment" href="mailto:msichova@outlook.com" title="Send email to me at outlook">msichova@outlook.com</a>,
+                &nbsp;,
+                WhatsApp: <a className="link-in-comment" href="https://wa.me/+61433881789" target="_blank" title="WhatsApp me">Marina Sichova</a>,
+                &nbsp;,
+                GitHub: <a className="link-in-comment" href="https://github.com/MarinaDotNet" target="_blank" title="Visit at GitHub">MarinaDotNet"</a>,
+                For more contact info/variants please look below.
+            </Comment>,
+            <><CodeKeyWords>public class</CodeKeyWords> Contact</>,
             <>&#123;</>,
-            <><Tab number={2} /><CodeKeyWords>public static void</CodeKeyWords> <ClassName>ShowContactInfo</ClassName>()</>,
+            <><Tab number={1} /><CodeKeyWords>public void</CodeKeyWords> <ClassName>ShowContactInfo</ClassName>()</>,
             <><Tab number={1} />&#123;</>,
-            <><Tab number={2} /><SingleComment>Let's Connect!</SingleComment></>,
+            <><Tab number={2} /><CodeIdentifier>MessageBox</CodeIdentifier>.<CodeIdentifier>Show</CodeIdentifier>(<CodeValues>"Let's connect."</CodeValues>);</>,
             <br/>,
-            <><Tab number={2} /><ClassName>Console</ClassName>.WriteLine(<CodeValues>"--------------------------"</CodeValues>);</>,
+            <><Tab number={2} /><ClassName>Console</ClassName>.WriteLine(<CodeValues>"----------"</CodeValues>);</>,
             <br/>,
-            <><Tab number={2} /><ClassName>Console</ClassName>.WriteLine(<CodeValues>"Email : <a href="mailto:msichova@outlook.com">msichova@outlook.com"</a></CodeValues>);</>,
+            <><Tab number={2} /><ClassName>Console</ClassName>.WriteLine(<CodeValues>"Email outlook: <a title="Send email to me at outlook" href="mailto:msichova@outlook.com" target="_blank">msichova@outlook.com"</a></CodeValues>);</>,
             <br/>,
-            <><Tab number={2} /><ClassName>Console</ClassName>.WriteLine(<CodeValues>"GitHub : <a href="https://github.com/MarinaDotNet">MarinaDotNet"</a></CodeValues>);</>,
+            <><Tab number={2} /><ClassName>Console</ClassName>.WriteLine(<CodeValues>"Email gmail: <a title="Send email to me at gmail" href="mailto:msichova.net@gmail.com" target="_blank">msichova.net@gmail.com"</a></CodeValues>);</>,
             <br/>,
-            <><Tab number={2} /><ClassName>Console</ClassName>.WriteLine(<CodeValues>"LinkedIn : <a href="https://www.linkedin.com/in/marina-sichova-333599281?originalSubdomain=au">Marina Sichova"</a></CodeValues>);</>,
+            <><Tab number={2} /><ClassName>Console</ClassName>.WriteLine(<CodeValues>"GitHub : <a title="Visit at GitHub" href="https://github.com/MarinaDotNet" target="_blank">MarinaDotNet"</a></CodeValues>);</>,
             <br/>,
-            <><Tab number={2} /><ClassName>Console</ClassName>.WriteLine(<CodeValues>"WhatsApp : <a href="https://wa.me/+61433881789">Marina Sichova"</a></CodeValues>);</>,
+            <><Tab number={2} /><ClassName>Console</ClassName>.WriteLine(<CodeValues>"LinkedIn : <a title="Visit at LinkedIn" href="https://www.linkedin.com/in/marina-sichova-333599281?originalSubdomain=au" target="_blank">Marina Sichova"</a></CodeValues>);</>,
             <br/>,
-            <><Tab number={2} /><ClassName>Console</ClassName>.WriteLine(<CodeValues>"Static Portfolio : <a href="https://marinadotnet.github.io/static">MarinaDotNet.github.io/static"</a></CodeValues>);</>,
+            <><Tab number={2} /><ClassName>Console</ClassName>.WriteLine(<CodeValues>"WhatsApp : <a title="WhatsApp me" href="https://wa.me/+61433881789" target="_blank">Marina Sichova"</a></CodeValues>);</>,
             <br/>,
-            <><Tab number={2} /><ClassName>Console</ClassName>.WriteLine(<CodeValues>"React-Vite Portfolio : <a href="https://marinadotnet.github.io">MarinaDotNet.github.io"</a></CodeValues>);</>,
+            <><Tab number={2} /><ClassName>Console</ClassName>.WriteLine(<CodeValues>"Facebook : <a title="Visit at Facebook" href="https://www.facebook.com/msichova/" target="_blank">Marina Sichova"</a></CodeValues>);</>,
+            <br/>,
+            <><Tab number={2} /><ClassName>Console</ClassName>.WriteLine(<CodeValues>"Static Portfolio : <a title="Look at my Static Portfolio" href="https://marinadotnet.github.io/static" target="_blank">MarinaDotNet.github.io/static"</a></CodeValues>);</>,
+            <br/>,
+            <><Tab number={2} /><ClassName>Console</ClassName>.WriteLine(<CodeValues>"React-Vite Portfolio : <a title="Look at my React-Vite Portfolio" href="https://marinadotnet.github.io" target="_blank">MarinaDotNet.github.io"</a></CodeValues>);</>,
             <br />,
-            <><Tab number={2} /><ClassName>Console</ClassName>.WriteLine(<CodeValues>"--------------------------"</CodeValues>);</>,
+            <><Tab number={2} /><ClassName>Console</ClassName>.WriteLine(<CodeValues>"----------"</CodeValues>);</>,
             <br/>,
-            <><Tab number={2} /><SingleComment>Feel free to reach out for collaborations, questions, or just to say hi.</SingleComment></>,
+            <><Tab number={2} /><SingleComment>Open to collaboration, questions, and new opportunities.</SingleComment></>,
             <><Tab number={1} />&#125;</>,
             <>&#125;</>
 
-        ]} />
+            ]} />
+        );
+    }
+
+    return(
+        <NumberedCodeBloc children={[
+            <span className="text-line hint-text">
+            Tip: switch between views to explore different perspectives.
+            </span>,
+            <br/>,
+            <span className="line-title">
+                Let's connect.
+            </span>,
+            <br/>,
+            <span className="text-line">---------------</span>,
+            <br/>,
+            <ul className="text-line line-list">
+                <span className="line-subtitle">Emails</span>
+                <li>Outlook: <a className="line-link" href="mailto:msichova@outlook.com" target="_blank" title="Send email to me at outlook">msichova@outlook.com</a></li>
+                <li>Gmail: <a className="line-link" href="mailto:msichova.net@gmail.com" target="_blank" title="Send email to me at gmail">msichova.net@gmail.com</a></li>
+            </ul>,
+            <br/>,
+            <ul className="text-line line-list">
+                <span className="line-subtitle">GitHub</span>
+                <li><a className="line-link" href="https://github.com/MarinaDotNet" title="Visit at GitHub" target="_blank">MarinaDotNet</a></li>
+            </ul>,
+            <br/>,
+            <ul className="text-line line-list">
+                <span className="line-subtitle">LinkedIn</span>
+                <li><a className="line-link" href="https://www.linkedin.com/in/marina-sichova-333599281?originalSubdomain=au" title="Visit at LinkedIn" target="_blank">Marina Sichova</a></li>
+            </ul>,
+            <br/>,
+            <ul className="text-line line-list">
+                <span className="line-subtitle">WhatsApp</span>
+                <li><a className="line-link" href="https://wa.me/+61433881789" title="WhatsApp me" target="_blank">Marina Sichova</a></li>
+            </ul>,
+            <br/>,
+            <ul className="text-line line-list">
+                <span className="line-subtitle">Facebook</span>
+                <li><a className="line-link" href="https://www.facebook.com/msichova/" title="Visit at Facebook" target="_blank">Marina Sichova</a></li>
+            </ul>,
+            <br/>,
+            <ul className="text-line line-list">
+                <span className="line-subtitle">Static Portfolio</span>
+                <li><a className="line-link" href="https://marinadotnet.github.io/static/" title="Look at my Static Portfolio" target="_blank">https://marinadotent.github.io/static/</a></li>
+            </ul>,
+            <br/>,
+            <ul className="text-line line-list">
+                <span className="line-subtitle">React-Vite Portfolio</span>
+                <li><a className="line-link" href="https://marinadotnet.github.io/" title="Look at my React-Vite Portfolio" target="_blank">https://marinadotnet.github.io/</a></li>
+            </ul>,
+            <br/>,
+            <span className="text-line">---------------</span>,
+            <br/>,
+            <span className="text-line">Open to collaboration, questions, and new opportunities.</span>
+        ]}/>
     );
+    
 }
